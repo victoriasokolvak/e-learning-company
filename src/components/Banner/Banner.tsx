db1d375349b2c9d4d97fc4e90899e1ed88264f0e
Victoria Sokolvak <sokolvak.victoria@gmail.com>
2025-01-22 18:51:32 +0200

    remove JSON file and import images directly for github pages test

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,12 +1,11 @@
 import { Slider } from '../Slider/Slider';
 import styles from './Banner.module.scss';
-import slides from '../../assets/mock.json';
 
 export const Banner = () => {
   return (
     <>
       <div className={styles.banner}>
-        <Slider slides={slides} />
+        <Slider />
       </div>
     </>
   );
--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -4,17 +4,21 @@ import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import email from '../../../public/img/Icons/email-icon.svg';
+import services from '../../../public/img/Slider/Services.png';
+import instructionalDesign from '../../../public/img/Slider/Instructional-Design.png';
+import eLearningDevelopment from '../../../public/img/Slider/eLearning-Development.png';
+import eLearningPackages from '../../../public/img/Slider/eLearning-Packages.png';
 
-type Slide = {
-  title: string;
-  image: string;
-};
+const slidesData = [
+  { title: 'Services', image: services, alt: 'Services' },
+  { title: 'Instructional Design', image: instructionalDesign, alt: 'Instructional Design' },
+  { title: 'eLearning Development', image: eLearningDevelopment, alt: 'eLearning Development' },
+  { title: 'eLearning Price Quote', image: services, alt: 'eLearning Price Quote' },
+  { title: 'eLearning Packages', image: eLearningPackages, alt: 'eLearning Packages' },
+  { title: 'Contact us', image: email, alt: 'Email', isEmail: true },
+];
 
-interface SliderProps {
-  slides: Slide[];
-}
-
-export const Slider = ({ slides }: SliderProps) => {
+export const Slider = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
@@ -31,16 +35,16 @@ export const Slider = ({ slides }: SliderProps) => {
           loop={true}
           speed={800}
         >
-          {slides.map((slide, index) => (
-            <SwiperSlide key={slide.title}>
-              {index === 5 ? (
+          {slidesData.map((slide, index) => (
+            <SwiperSlide key={index}>
+              {slide.isEmail ? (
                 <div className={styles.email_slide}>
-                  <img src={email} alt="Email" className={styles.email_slide__icon} />
-                  <h3 className={styles.email_slide__title}>Contact us</h3>
+                  <img src={slide.image} alt={slide.alt} className={styles.email_slide__icon} />
+                  <h3 className={styles.email_slide__title}>{slide.title}</h3>
                 </div>
               ) : (
                 <div className={styles.slide}>
-                  <img src={slide.image} alt={slide.title} className={styles.slide__image} />
+                  <img src={slide.image} alt={slide.alt} className={styles.slide__image} />
                   <h3 className={styles.slide__title}>{slide.title}</h3>
                 </div>
               )}
