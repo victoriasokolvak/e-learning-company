20bb015aa19d99c56c3024daf35940042a4f646f
Victoria Sokolvak <sokolvak.victoria@gmail.com>
2025-01-22 19:26:56 +0200

    refactor slider component: move slide data to a separate file

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -3,20 +3,7 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import email from '../../../public/img/Icons/email-icon.svg';
-import services from '../../../public/img/Slider/Services.png';
-import instructionalDesign from '../../../public/img/Slider/Instructional-Design.png';
-import eLearningDevelopment from '../../../public/img/Slider/eLearning-Development.png';
-import eLearningPackages from '../../../public/img/Slider/eLearning-Packages.png';
-
-const slidesData = [
-  { title: 'Services', image: services, alt: 'Services' },
-  { title: 'Instructional Design', image: instructionalDesign, alt: 'Instructional Design' },
-  { title: 'eLearning Development', image: eLearningDevelopment, alt: 'eLearning Development' },
-  { title: 'eLearning Price Quote', image: services, alt: 'eLearning Price Quote' },
-  { title: 'eLearning Packages', image: eLearningPackages, alt: 'eLearning Packages' },
-  { title: 'Contact us', image: email, alt: 'Email', isEmail: true },
-];
+import { slidesData } from '../../constants/slidesData';
 
 export const Slider = () => {
   return (
---
+++ src/constants/slidesData.ts
@@ -0,0 +1,14 @@
+import email from '../../public/img/Icons/email-icon.svg';
+import services from '../../public/img/Slider/Services.png';
+import instructionalDesign from '../../public/img/Slider/Instructional-Design.png';
+import eLearningDevelopment from '../../public/img/Slider/eLearning-Development.png';
+import eLearningPackages from '../../public/img/Slider/eLearning-Packages.png';
+
+export const slidesData = [
+  { title: 'Services', image: services, alt: 'Services' },
+  { title: 'Instructional Design', image: instructionalDesign, alt: 'Instructional Design' },
+  { title: 'eLearning Development', image: eLearningDevelopment, alt: 'eLearning Development' },
+  { title: 'eLearning Price Quote', image: services, alt: 'eLearning Price Quote' },
+  { title: 'eLearning Packages', image: eLearningPackages, alt: 'eLearning Packages' },
+  { title: 'Contact us', image: email, alt: 'Email', isEmail: true },
+];
